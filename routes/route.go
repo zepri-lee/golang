96b14ea4d14fbb6fdb1935acdc561fd90b0c7e8a
DESKTOP-CH45GN4\ZEPRI-E5570 <hoik.lee@aoroa.ai>
2024-03-24 15:35:56 +0900

    corsConfig 추가

--- routes/route.go
+++ routes/route.go
@@ -2,11 +2,14 @@ package routes
 
 import (
 	"gin-gonic-gorm/controllers"
+	"time"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 func InitRoute(app *gin.Engine) {
+	app.Use(corsConfig())
 
 	api := app.Group("/api")
 	{
@@ -15,4 +18,18 @@ func InitRoute(app *gin.Engine) {
 		api.DELETE("/stock", controllers.DeleteAllStock)
 		api.POST("/sale", controllers.AddSale)
 	}
+}
+
+func corsConfig() gin.HandlerFunc {
+	return cors.New(cors.Config{
+		AllowOrigins:     []string{"*"},
+		AllowMethods:     []string{"GET", "PUT", "PATCH", "POST", "DELETE", "OPTIONS"},
+		AllowHeaders:     []string{"Origin", "Authorization", "Accept-Language", "Content-Type"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		AllowOriginFunc: func(origin string) bool {
+			return origin == "https://github.com"
+		},
+		MaxAge: 12 * time.Hour,
+	})
 }
