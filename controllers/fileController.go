7306a89cd77378c0f5346f4e66c93231f8067c0a
AzureAD\HoikLee <hoik.lee@aoroa.ai>
2024-03-26 17:10:18 +0900

    파일 업로드 파일명 변경 저장

--- controllers/fileController.go
+++ controllers/fileController.go
@@ -2,7 +2,10 @@ package controllers
 
 import (
 	"fmt"
+	"gin-gonic-gorm/utils"
 	"net/http"
+	"path/filepath"
+	"time"
 
 	"github.com/gin-gonic/gin"
 )
@@ -17,14 +20,17 @@ func HandleUploadFile(ctx *gin.Context) {
 		return
 	}
 
-	//	file, errFile := fileHeader.Open()
-	//	if errFile != nil {
-	//		panic(errFile)
-	//	}
+	// file, errFile := fileHeader.Open()
+	// if errFile != nil {
+	// 	panic(errFile)
+	// }
 
-	//	extentionFile := filepath.Ext(fileHeader.Filename)
-	//	filename :=
-	errUpload := ctx.SaveUploadedFile(fileHeader, fmt.Sprintf("./public/files/%s", fileHeader.Filename))
+	extentionFile := filepath.Ext(fileHeader.Filename)
+	fmt.Println("extentionFile : " + extentionFile)
+	currentTime := time.Now().UTC().Format("20060101")
+	filename := fmt.Sprintf("%s-%s%s", currentTime, utils.RandomString(5), extentionFile)
+
+	errUpload := ctx.SaveUploadedFile(fileHeader, fmt.Sprintf("./public/files/%s", filename))
 	if errUpload != nil {
 		ctx.JSON(http.StatusInternalServerError, gin.H{
 			"message": errUpload.Error(),
---
+++ utils/util.go
@@ -0,0 +1,14 @@
+package utils
+
+import "math/rand"
+
+var charset = []byte("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789")
+
+func RandomString(n int) string {
+	b := make([]byte, n)
+	for i := range b {
+		b[i] = charset[rand.Intn(len(charset))]
+	}
+
+	return string(b)
+}
